8a7f9c8f79f02d41894f9d323f0bac7dd10b691e
weizhidaoshuo <weizhidaoshuo@weizhidaoshuodeMac-mini.local>
2017-07-21 00:18:49 +0800

    add develop branch and init project

---
+++ RongCloudSingleChat/RongCloudSingleChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RongCloudSingleChat
+//
+//  Created by weizhidaoshuo on 2017/7/20.
+//  Copyright © 2017年 weizhidaoshuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RongCloudSingleChat/RongCloudSingleChat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RongCloudSingleChat
+//
+//  Created by weizhidaoshuo on 2017/7/20.
+//  Copyright © 2017年 weizhidaoshuo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
